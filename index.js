1e32fb095c2a2fd4e58c32a672e570dc009d5e66
louis baptiste <louisvis@ymail.com>
2020-03-28 05:05:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 funtion justInvoke (fn){
   return fn();
 }
+
+funtion setThisWithCall (fn, thisValue, argument){
+  return fn.call(thisValue, argument);
+}
+
+funtion setThisWithApply (fn, thisValue, argument){
+  return fn.apply(thisValue, ourArguments);
+  
+}
+
+funtion returnNewFunctionOf(functionToBeCopied, thisValue){
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
+
